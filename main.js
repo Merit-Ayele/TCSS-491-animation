4d33dfbf6d3f2fe5cacba3a1a1ce156df4fb9266
Merit Mamo <meritayele@gmail.com>
2022-01-22 15:39:30 -0800

    added text in canvas

--- main.js
+++ main.js
@@ -4,8 +4,16 @@ import Player from "./pirate.js";
 const canvas = document.createElement("canvas");
 document.body.append(canvas);
 const ctx = canvas.getContext("2d");
-canvas.width = 1763 / 1.5;
-canvas.height = 1766 / 1.5;
+canvas.width = 2000 ;
+canvas.height = 1766;
+
+ctx.font = "40px Georgia";
+ctx.fillText("Right-arrow: walk", 1200, 70);
+ctx.fillText("Shift-right + Right-arrow: run",1200,130);
+ctx.fillText("Down-arrow: attack",1200,190);
+ctx.fillText("Space: jump",1200,250);
+ctx.fillText("D: dead",1200,310);
+ctx.fillText("R: restore after dead",1200,370);
 
 const background = img("pbg.png");
 const player = new Player();
@@ -16,3 +24,4 @@ function game() {
 }
 
 setInterval(game, 1000 / 60);
+ 
